a6d5794ff9830351e577f7f249321a9be89b4264
iradukun <mustaphairadukunda@gmail.com>
2023-12-21 23:03:39 +0200

    the  upper section and footer

---
+++ src/app/components/featured-page/component-82-property-1-defaul/component-82-property-1-defaul.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+
+/* @figmaId 9:8512 */
+@Component({
+  selector: 'cl-component-82-property-1-defaul',
+  templateUrl: './component-82-property-1-defaul.component.html',
+  styleUrls: ['./component-82-property-1-defaul.component.scss'],
+})
+export class Component82Property1DefaulComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
---
+++ src/app/components/featured-page/mac-book-pro-14-12.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+
+/* @figmaId 9:9710 */
+@Component({
+  selector: 'cl-mac-book-pro-14-12',
+  templateUrl: './mac-book-pro-14-12.component.html',
+  styleUrls: ['./mac-book-pro-14-12.component.scss'],
+})
+export class MacBookPro1412Component implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
